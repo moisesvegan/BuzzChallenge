38035fd34435b5d1da8f50b7fdab4152fa174a8e
moises.vega <moises.vega@innew.la>
2024-09-30 12:54:41 -0300

    Buzz challenge

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import ticketsReducer from './store/ticketsSlice';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
-// Crear el store de Redux
+
 const store = configureStore({
   reducer: {
     tickets: ticketsReducer,
   },
 });
 
-// Crear el root usando React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
